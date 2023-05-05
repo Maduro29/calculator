8855cdbd5fa08d5d477c97b5e6a116f251089a1e
Maduro29 <manhdungtran.vnuuet@gmail.com>
2023-05-05 14:48:38 +0700

    create display and button panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Display />
+        <ButtonPanel />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+class ButtonPanel extends React.Component {
+    render() {
+        return (
+            <div>
+                <Button />
+            </div>
+        )
+    }
+}
+
+export default ButtonPanel;
